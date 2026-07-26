9a24b6f38bdf09d7e253fe8e1f651538a66d356d
luoxiaozhuang <luoxiaozhuang@outlook.com>
2026-07-26 19:10:49 +0800

    Replay FT samples after barrier policy update

--- services/olds-api/jojo_olds_api/publisher_specs.py
+++ services/olds-api/jojo_olds_api/publisher_specs.py
@@ -131,7 +131,7 @@ PUBLISHER_SPECS = {
     ),
     "ft": PublisherSpec(
         publisher="ft",
-        parser_version="ft-parser/0.8.0",
+        parser_version="ft-parser/0.8.1",
         domains=("ft.com", "www.ft.com"),
         default_language="en",
         edition="global",
