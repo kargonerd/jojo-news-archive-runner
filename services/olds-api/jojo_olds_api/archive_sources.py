6acffeb8a041c04aaa50c0504f41a39a625be0e9
luoxiaozhuang <luoxiaozhuang@outlook.com>
2026-08-06 22:26:38 +0800

    fix(axios): cover legacy section date URLs

--- services/olds-api/jojo_olds_api/archive_sources.py
+++ services/olds-api/jojo_olds_api/archive_sources.py
@@ -146,8 +146,13 @@ ARCHIVE_SOURCE_SPECS = {
     "axios": ArchiveSourceSpec(
         publisher="axios",
         canonical_host="www.axios.com",
-        wayback_patterns=("axios.com/{year}/*", "www.axios.com/{year}/*"),
-        accepted_path_patterns=_patterns(r"^/20\d{2}/"),
+        wayback_patterns=(
+            "axios.com/{year}/*",
+            "www.axios.com/{year}/*",
+            "axios.com/*/{year}/*",
+            "www.axios.com/*/{year}/*",
+        ),
+        accepted_path_patterns=_patterns(r"^/(?:[^/]+/)?20\d{2}/"),
         rejected_path_patterns=_patterns(r"^/(?:newsletters?|signup|about)(?:/|$)"),
     ),
     "npr": ArchiveSourceSpec(
